70bfb590b2e99eae266614056b4eb17205a0c422
Bert Guns <bertguns@pm.me>
2020-12-28 17:16:59 +0100

    Change setting of environment variables to be -gx

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,11 +3,11 @@ if not test -d ~/.config/omf
     source ~/.setup_scripts/fish-shell-setup.fish
 end
 
-set PATH ~/.local/bin ~/.cargo/bin $PATH
+set -gx PATH ~/.local/bin ~/.cargo/bin $PATH
 
 set -g theme_nerd_fonts yes
 set -g theme_color_scheme gruvbox
 
-set -Ux LS_COLORS (dircolors ~/.config/.dircolors | head -n 1 | sed 's/LS_COLORS=\'//' | sed 's/\';//')
+set -gx LS_COLORS (dircolors ~/.config/.dircolors | head -n 1 | sed 's/LS_COLORS=\'//' | sed 's/\';//')
 
 cd /mnt/c/Users/Bert/Development
